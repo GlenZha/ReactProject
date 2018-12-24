b7e5d7ce104262f246f8b6a7937fdf669546cdc0
GlenZha <36531415+GlenZha@users.noreply.github.com>
2018-12-24 17:27:22 +0800

    代码优化

--- todolist/src/ToList.js
+++ todolist/src/ToList.js
@@ -8,7 +8,10 @@ class ToList extends Component{
         this.state={
             inputValue:'',
             list:['react','vue']
-        }
+        };
+        this.handleInputChange=this.handleInputChange.bind(this);
+        this.handBtnClick=this.handBtnClick.bind(this);
+        this.handleItemDele=this.handleItemDele.bind(this)
     }
     render() {
         return (<Fragment>
@@ -16,46 +19,61 @@ class ToList extends Component{
             htmlFor='insert'等于for，点击相当于点击了id=insert的元素
             */}
             <label htmlFor='insert'>输入内容</label>
-            <input id='insert' className="input" value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/><button onClick={this.handBtnClick.bind(this)}>提交</button>
+            <input id='insert' className="input" value={this.state.inputValue} onChange={this.handleInputChange}/><button onClick={this.handBtnClick}>提交</button>
             <ul>
                 <li>英语</li>
                 <li>{this.state.inputValue}</li>
-                {
-                    this.state.list.map((item,index)=>{
-                        //dangerouslySetInnerHTML={{__html:item}}将输入文本以HTML解析
-                        //<li key={index} onClick={this.handleItemDele.bind(this,index)} dangerouslySetInnerHTML={{__html:item}}></li>
-                        //<ToItem content={item}/>通过属性来向子组件传递数据
-                        return (<div>
-                            {/*bind(this)将父组件的this绑定给子组件*/}
-                            <ToItem content={item} index={index} handleItemDele={this.handleItemDele.bind(this)}/>
-                        </div>)
-                    })
-                }
+                {this.getToItem()}
             </ul>
         </Fragment>)
+    }
+    getToItem(){
+       return this.state.list.map((item,index)=>{
+            //dangerouslySetInnerHTML={{__html:item}}将输入文本以HTML解析
+            //<li key={index} onClick={this.handleItemDele.bind(this,index)} dangerouslySetInnerHTML={{__html:item}}></li>
+            //<ToItem content={item}/>通过属性来向子组件传递数据
+            return (
+                /*bind(this)将父组件的this绑定给子组件*/
+                <ToItem key={index} content={item} index={index} handleItemDele={this.handleItemDele}/>
+            )
+        })
     }
     handleInputChange(e){
         //this.state.inputValue=e.target.value;
         //通过setState方法更改state数据
-        this.setState({
-            inputValue:e.target.value,
-            }
-        );
+        const value=e.target.value;
+        this.setState(()=>({
+            inputValue:value
+        }));
+        // this.setState({
+        //     inputValue:e.target.value,
+        //     }
+        // );
         console.log(e.target.value);
     }
     handBtnClick(){
-            this.setState({
-                list:[...this.state.list,this.state.inputValue],
+            // this.setState({
+            //     list:[...this.state.list,this.state.inputValue],
+            //     inputValue:''
+            // })
+        this.setState((prevState)=>{
+            return {
+                list:[...prevState.list,prevState.inputValue],
                 inputValue:''
-            })
+            }
+        })
     }
     handleItemDele(index){
         console.log(index);
         //不允许直接修改sate的值，拷贝出一个副本进行修改
-        const list=[...this.state.list];
-        list.splice(index,1);
-        this.setState({
-            list:list
+        // list.splice(index,1);
+        // this.setState({
+        //     list:list
+        // })
+        this.setState((prevState)=>{
+            const list=[...prevState.list];
+            list.splice(index,1);
+            return {list}
         })
     }
 }
