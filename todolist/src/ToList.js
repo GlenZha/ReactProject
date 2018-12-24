e82f2b1430462c3a0acedc8a00f18ae50d33bcca
GlenZha <36531415+GlenZha@users.noreply.github.com>
2018-12-24 14:53:18 +0800

    new react project

---
+++ todolist/src/ToList.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class ToList extends Component{
+    render() {
+        return (<div>
+            <input/><button>提交</button>
+            <ul>
+                <li>英语</li>
+            </ul>
+        </div>)
+    }
+}
+export default ToList;
