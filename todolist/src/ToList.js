065dccb70da9fd4c316612e7cedf0e48303ac742
GlenZha <36531415+GlenZha@users.noreply.github.com>
2018-12-24 15:45:49 +0800

    事件绑定和删除

--- todolist/src/ToList.js
+++ todolist/src/ToList.js
@@ -1,13 +1,50 @@
-import React,{Component} from "react";
-
+import React,{Component,Fragment} from "react";
+//Fragment占位符，可以让标签隐藏掉，少个包裹的标签
 class ToList extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            inputValue:'',
+            list:['react','vue']
+        }
+    }
     render() {
-        return (<div>
-            <input/><button>提交</button>
+        return (<Fragment>
+            {/*bind(this)绑定this的指向*/}
+            <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/><button onClick={this.handBtnClick.bind(this)}>提交</button>
             <ul>
                 <li>英语</li>
+                <li>{this.state.inputValue}</li>
+                {
+                    this.state.list.map((item,index)=>{
+                        return <li key={index} onClick={this.handleItemDele.bind(this,index)}>{item}</li>
+                    })
+                }
             </ul>
-        </div>)
+        </Fragment>)
+    }
+    handleInputChange(e){
+        //this.state.inputValue=e.target.value;
+        //通过setState方法更改state数据
+        this.setState({
+            inputValue:e.target.value,
+            }
+        );
+        console.log(e.target.value);
+    }
+    handBtnClick(){
+            this.setState({
+                list:[...this.state.list,this.state.inputValue],
+                inputValue:''
+            })
+    }
+    handleItemDele(index){
+        console.log(index);
+        const list=[...this.state.list];
+        list.splice(index,1);
+        this.setState({
+            list:list
+        })
     }
 }
 export default ToList;
