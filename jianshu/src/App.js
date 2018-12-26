d191dca706f5fe383a14dda8634e8553065e3b3f
GlenZha <36531415+GlenZha@users.noreply.github.com>
2018-12-26 15:29:33 +0800

    引用redux控制动画效果

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -2,13 +2,18 @@ import React, { Component,Fragment } from 'react';
 import {Globalstyle} from './style.js'
 import Header from './common/header'
 import {Iconfont} from  './statics/iconfont/iconfont'
+//引入redux，使用store
+import {Provider} from 'react-redux';
+import store from './store'
+
 
 class App extends Component {
   render() {
     return (<Fragment>
                 <Globalstyle/>
                 <Iconfont/>
-                <Header/>
+        <Provider store={store}><Header/></Provider>
+
         </Fragment>);
   }
 }
--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,4 +1,5 @@
-import React,{Component} from 'react';
+import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 import {
     HeaderWrapper,
     Logo,
@@ -9,26 +10,57 @@ import {
     SearchWrapper,
     Button
 } from "./style";
+import {connect} from 'react-redux'
 
-class Header extends Component{
-    render() {
-        return (<HeaderWrapper>
-            <Logo/>
-            <Nav>
-                <NavItem className='left active'>首页</NavItem>
-                <NavItem className='left'>下载App</NavItem>
-                <NavItem className='right'>登录</NavItem>
-                <NavItem className='right'><i className="iconfont">&#xe636;</i></NavItem>
-                <SearchWrapper>
-                    <NavSearch></NavSearch>
-                    <i className="iconfont">&#xe60b;</i>
-                </SearchWrapper>
-            </Nav>
-            <Addition>
-                <Button className='writting'><i className="iconfont">&#xe62d;</i>写文章</Button>
-                <Button className='reg'>注册</Button>
-            </Addition>
-            </HeaderWrapper>)
+const Header =(props)=>{
+    return (<HeaderWrapper>
+        <Logo/>
+        <Nav>
+            <NavItem className='left active'>首页</NavItem>
+            <NavItem className='left'>下载App</NavItem>
+            <NavItem className='right'>登录</NavItem>
+            <NavItem className='right'><i className="iconfont">&#xe636;</i></NavItem>
+            <SearchWrapper>
+                <CSSTransition
+                    in={props.focused}
+                    timeout={300}
+                    classNames={"slide"}>
+                    <NavSearch className={props.focused ?'focused':''}
+                               onFocus={props.handleInputFocus}
+                               onBlur={props.handleInputBlur}
+                    />
+                </CSSTransition>
+
+                <i className={props.focused ?'focused iconfont':'iconfont'}>&#xe60b;</i>
+            </SearchWrapper>
+        </Nav>
+        <Addition>
+            <Button className='writting'><i className="iconfont">&#xe62d;</i>写文章</Button>
+            <Button className='reg'>注册</Button>
+        </Addition>
+    </HeaderWrapper>)
+
+};
+//把state映射到props
+const mapStateToProps=(state)=>{
+      return {
+          focused:state.focused
+      }
+};
+const mapDispatchToProps=(dispatch)=>{
+    return {
+        handleInputFocus(){
+            const action={
+                type:'search_focus'
+            };
+            dispatch(action);
+        },
+        handleInputBlur(){
+            const action={
+                type:'search_blur'
+            };
+            dispatch(action);
+        }
     }
-}
-export default Header;
+};
+export default  connect(mapStateToProps,mapDispatchToProps)(Header);
