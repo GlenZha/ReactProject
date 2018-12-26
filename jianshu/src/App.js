ed8df465e88b6249c92a6801a421d51dfcf13d66
GlenZha <36531415+GlenZha@users.noreply.github.com>
2018-12-26 08:12:03 +0800

    加入全局样式（百度reset.css）

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import {Globalstyle} from './style.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Globalstyle/>
         hello
       </div>
     );
